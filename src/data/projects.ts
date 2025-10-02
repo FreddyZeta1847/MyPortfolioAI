4fde275fc861c294e2f012f94947bdf741282f34
Federico Santini <santinifederico06@gmail.com>
2025-10-02 11:05:40 +0200

    change the WebScrapingApp information

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projects: Project[] = [
     id: 1,
     title: 'Interactive Renewable Energy Map',
     description: 'An interactive map displaying the distribution of renewable energy sources across Italy, built with Folium (Python). Integrates georeferenced data with detailed energy type information retrieved from the Overpass API.',
-    technologies: ['Python', 'Folium', 'Data Visualization', "Plotly", "Flask", "Overpass API"],
+    technologies: ['Python', 'Folium', 'Data Visualization', "Plotly", "Flask", "Overpass API", "Claude 4 Sonnet"],
     imageUrl: 'https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     githubUrl: 'https://github.com/FreddyZeta1847/EnergyPro'
   },
@@ -21,7 +21,7 @@ export const projects: Project[] = [
     id: 3,
     title: 'Web Scraping Application',
     description: 'A powerful web scraping application designed to extract and process data from websites efficiently. Features automated data collection, parsing, and storage capabilities with a user-friendly interface.',
-    technologies: ['Python', 'BeautifulSoup', 'Selenium', 'Pandas'],
+    technologies: ['Ollama', 'Nodejs', 'Mongodb', 'Claude Code', "Express"],
     imageUrl: 'https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     githubUrl: 'https://github.com/FreddyZeta1847/Web-Scraping-App'
   }
